1cb8069db240ce94acc2c65ffe0bce292fc34ffe
KVLDK <35120315+KVLDK@users.noreply.github.com>
2018-01-22 15:10:26 -0500

    Update BattleOperations.java

--- Java Bean Forest/BattleOperations.java
+++ Java Bean Forest/BattleOperations.java
@@ -1,13 +1,15 @@
 /**
- Vahldieck, Kara
+ V, Kara
  COP-3252
  Assignment 5
  3/21/17
+ 
+ Common methods to be implemented in both Knight and Enemy abstract/subclasses
+ 
  */
 
 public interface BattleOperations {
 
-    //common methods to be implemented in both Knight and Enemy abstract/subclasses
     int takeDamage(int value, String oppWeapon) throws InvalidDamageException;
     int fight();
 }
