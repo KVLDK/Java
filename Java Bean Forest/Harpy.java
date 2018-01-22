b9f785291a57d389d86cc9e6f0d558832a3be71c
KVLDK <35120315+KVLDK@users.noreply.github.com>
2018-01-22 15:12:28 -0500

    Add files via upload

---
+++ Java Bean Forest/Harpy.java
@@ -0,0 +1,36 @@
+/**
+ Vahldieck, Kara
+ COP-3252
+ Assignment 5
+ 3/21/17
+ */
+
+import java.util.Random;
+
+public class Harpy extends Enemy {
+
+    Random randomNumber = new Random();
+
+    public Harpy()
+    {
+        setName("Harpy");
+        setWeapon("Claws");
+        setArmor("Wings");
+        setHealth(15 + randomNumber.nextInt(10));
+    }
+
+    //Harpy's version of fight method
+    @Override
+    public int fight()
+    {
+        int damage = (10 + randomNumber.nextInt(11));
+        return damage;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("\n*****\nCreature: %s\nWeapon: %s\nArmor: %s\nHealth: %d\n*****",
+            getName(), getWeapon(), getArmor(), getHealth());
+    }
+}
