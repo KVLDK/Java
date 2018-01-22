b9fe4b42e0659ee8633597e811e68eb714898d91
KVLDK <35120315+KVLDK@users.noreply.github.com>
2018-01-22 15:14:06 -0500

    Add files via upload

---
+++ Java Bean Forest/Minotaur.java
@@ -0,0 +1,36 @@
+/**
+ Vahldieck, Kara
+ COP-3252
+ Assignment 5
+ 3/21/17
+ */
+
+import java.util.Random;
+
+public class Minotaur extends Enemy {
+
+    Random randomNumber = new Random();
+
+    public Minotaur()
+    {
+        setName("Minotaur");
+        setWeapon("Horns");
+        setArmor("Brute Strength");
+        setHealth(15 + randomNumber.nextInt(10));
+    }
+
+    //Minotaur's version of fight method
+    @Override
+    public int fight()
+    {
+        int damage = (10 + randomNumber.nextInt(10));
+        return damage;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("\n*****\nCreature: %s\nWeapon: %s\nArmor: %s\nHealth: %d\n*****",
+            getName(), getWeapon(), getArmor(), getHealth());
+    }
+}
