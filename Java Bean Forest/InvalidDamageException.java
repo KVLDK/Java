77843746a94b460bd2bbbc0fddfda81cdf736d4a
KVLDK <35120315+KVLDK@users.noreply.github.com>
2018-01-22 15:13:00 -0500

    Add files via upload

---
+++ Java Bean Forest/InvalidDamageException.java
@@ -0,0 +1,13 @@
+/**
+ Vahldieck, Kara
+ COP-3252
+ Assignment 5
+ 3/21/17
+ */
+
+public class InvalidDamageException extends Exception {
+
+    //will catch if negative value passed as a method argument
+    public InvalidDamageException()
+    {}
+}
